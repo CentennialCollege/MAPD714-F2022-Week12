7da20a5f69d30fec3cff0de4b44246734c5b9742
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-30 21:07:13 -0500

    initial commit

---
+++ MAPD714-F2022-Week12/MAPD714_F2022_Week12App.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD714_F2022_Week12App.swift
+//  MAPD714-F2022-Week12
+//
+//  Created by Tom Tsiliopoulos on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct MAPD714_F2022_Week12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
